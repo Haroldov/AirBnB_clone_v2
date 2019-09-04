c71293485f4b45133083fa6741b199cee40348f0
root <hvelez020@gmail.com>
2019-09-04 21:32:57 +0000

    [add] task #11

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -2,36 +2,16 @@
 """ starts a Flask web application """
 
 from flask import Flask, render_template
-from os import environ
 import models
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-@app.route("/states/<ids>", strict_slashes=False)
-def listStates(ids=None):
+@app.route("/hbnb_filters", strict_slashes=False)
+def listStates():
     """ return HTML template for listing states and cities """
 
-    states = models.storage.all("State")
-    cities = None
-    title = "States"
-    if ids is not None and "State." + str(ids) in states:
-        if 'HBNB_TYPE_STORAGE' in environ and\
-           environ['HBNB_TYPE_STORAGE'] == 'db':
-            title = "State: " + states["State." + str(ids)].name
-            cities = states["State." + str(ids)].cities
-            states = "yes"
-        else:
-            title = "State: " + states["State." + str(ids)].name
-            states = "yes"
-            cities = states["State." + str(ids)].cities()
-    elif ids is not None and "State." + str(ids) not in states:
-        states = None
-        title = "Not found!"
     return render_template('10-hbnb_filters.html',
-                           states=states,
-                           title=title,
-                           cities=cities)
+                           states=models.storage.all("State"))
 
 
 @app.teardown_appcontext
